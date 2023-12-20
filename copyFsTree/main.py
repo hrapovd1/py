e06cac28bf2f76160b48759946b6c58583a31b69
Dmitry Khrapov <hrapovd@gmail.com>
2023-12-20 08:37:44 +0300

    cp fs tree programm add

---
+++ copyFsTree/main.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+
+src_dir = '/etc'
+dst_dir = '/home/dima/etc'
+
+
+def mk_tree(src: str, dst: str) -> None:
+    src_glob = Path(src_dir).glob('*')
+    dst_path = Path(dst_dir)
+    for path in src_glob:
+        curr_path = dst_path / path.relative_to(src_dir)
+        if path.is_dir():
+            curr_path.mkdir(exist_ok=True)
+        elif path.is_file():
+            curr_path.touch()
+
+
+if __name__ == "__main__":
+    Path(dst_dir).mkdir()
+    mk_tree(src_dir, dst_dir)
